9cbab548ff07df3b03b96434c2c1147ff2b90b1a
Spyros Synodinos <spyros@giantswarm.io>
2024-12-18 12:50:48 +0200

    security-context patch

--- sync/sync.sh
+++ sync/sync.sh
@@ -16,6 +16,7 @@ vendir sync
 ./sync/patches/image-registry/patch.sh
 ./sync/patches/pss-comply/patch.sh
 ./sync/patches/team-label/patch.sh
+./sync/patches/security-context/patch.sh
 ./sync/patches/values/patch.sh
 
 HELM_DOCS="docker run --rm -u $(id -u) -v ${PWD}:/helm-docs -w /helm-docs jnorwood/helm-docs:v1.11.0"
@@ -35,7 +36,7 @@ for f in $(git --no-pager diff --no-exit-code --no-color --no-index vendor/gatew
         set +e
         set -x
 
-        git --no-pager diff --no-exit-code --no-color --no-index $base_file "${f}" \
+        git --no-pager diff --no-exit-code --no-color --no-index "$base_file" "${f}" \
                 > "./diffs/${f//\//__}.patch" # ${f//\//__} replaces all "/" with "__"
 
         ret=$?
