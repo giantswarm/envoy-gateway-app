977bd1b98243cbe965d61793e47617a5b0542bcf
Matías Charrière <matias@giantswarm.io>
2024-12-19 17:14:09 +0100

    update 1.2.1 (#27)
    * Update patches with changes for upgrade
    
    Signed-off-by: Matias Charriere <matias@giantswarm.io>
    
    * Upgrade envoy-gateway to 1.2.1
    
    Signed-off-by: Matias Charriere <matias@giantswarm.io>
    
    * run sync
    
    Signed-off-by: Puja Abbassi <puja108@gmail.com>
    
    * set readOnlyRootFilesystem: true
    
    * revert
    
    * security context fix
    
    * security-context patch
    
    * security-context patch fix
    
    * security-context patch remove
    
    * doc updates
    
    * typo fix
    
    * fix image registry patch
    
    Signed-off-by: Puja Abbassi <puja108@gmail.com>
    
    * add repo to image registry patch
    
    Signed-off-by: Puja Abbassi <puja108@gmail.com>
    
    * CiliumNetworkPolicies & CiliumNetworkPolicies
    
    * netpol directory fix
    
    * labels fix
    
    * helm pre-hook annotations
    
    * more more access to api-server from certgen
    
    * Add change to namespace value
    
    * revert broken chart
    
    * Fix script for sync
    
    * Fix namespace sync
    
    ---------
    
    Signed-off-by: Matias Charriere <matias@giantswarm.io>
    Signed-off-by: Puja Abbassi <puja108@gmail.com>
    Co-authored-by: Puja <puja108@gmail.com>
    Co-authored-by: Spyros Synodinos <spyros@giantswarm.io>
    Co-authored-by: pipo02mix <fernando@giantswarm.io>

--- sync/sync.sh
+++ sync/sync.sh
@@ -12,11 +12,16 @@ set -x
 vendir sync
 { set +x; } 2>/dev/null
 
+# Remove trailing whitespace end of lines (hack to fix vendir bug)
+find vendor/ -type f -exec sed -i 's/[[:space:]]*$//' {} \;
+
 # Patches
 ./sync/patches/image-registry/patch.sh
 ./sync/patches/pss-comply/patch.sh
 ./sync/patches/team-label/patch.sh
+./sync/patches/namespace-values/patch.sh
 ./sync/patches/values/patch.sh
+./sync/patches/network-policies/patch.sh
 
 HELM_DOCS="docker run --rm -u $(id -u) -v ${PWD}:/helm-docs -w /helm-docs jnorwood/helm-docs:v1.11.0"
 $HELM_DOCS --template-files=sync/readme.gotmpl -g helm/envoy-gateway -f values.yaml -o README.md
@@ -35,7 +40,7 @@ for f in $(git --no-pager diff --no-exit-code --no-color --no-index vendor/gatew
         set +e
         set -x
 
-        git --no-pager diff --no-exit-code --no-color --no-index $base_file "${f}" \
+        git --no-pager diff --no-exit-code --no-color --no-index "$base_file" "${f}" \
                 > "./diffs/${f//\//__}.patch" # ${f//\//__} replaces all "/" with "__"
 
         ret=$?
