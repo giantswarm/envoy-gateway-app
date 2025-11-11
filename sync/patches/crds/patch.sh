9ef259564d54b945adaf570b3bb27ff2db58b024
Matías Charrière <matias@giantswarm.io>
2025-11-11 11:17:21 +0100

    Install CRDs as templates with keep annotation (#91)
    * Use kubectlApplyJob for CRDs management
    
    Signed-off-by: Matias Charriere <matias@giantswarm.io>
    
    * Update patch
    
    Signed-off-by: Matias Charriere <matias@giantswarm.io>
    
    * remove empty line
    
    Signed-off-by: Matias Charriere <matias@giantswarm.io>
    
    * remove diff for Chart.lock
    
    Signed-off-by: Matias Charriere <matias@giantswarm.io>
    
    * bump kubectl-apply-job chart
    
    Signed-off-by: Matias Charriere <matias@giantswarm.io>
    
    * update readme
    
    Signed-off-by: Matias Charriere <matias@giantswarm.io>
    
    * update kubectl-apply-job
    
    Signed-off-by: Matias Charriere <matias@giantswarm.io>
    
    * one more time
    
    Signed-off-by: Matias Charriere <matias@giantswarm.io>
    
    * fix file names
    
    Signed-off-by: Matias Charriere <matias@giantswarm.io>
    
    * use for and mv
    
    Signed-off-by: Matias Charriere <matias@giantswarm.io>
    
    * fix name in values
    
    Signed-off-by: Matias Charriere <matias@giantswarm.io>
    
    * update kubectl-apply-job
    
    Signed-off-by: Matias Charriere <matias@giantswarm.io>
    
    * fix readme
    
    Signed-off-by: Matias Charriere <matias@giantswarm.io>
    
    * add doc sep
    
    Signed-off-by: Matias Charriere <matias@giantswarm.io>
    
    * sync/sync
    
    Signed-off-by: Matias Charriere <matias@giantswarm.io>
    
    * one more try
    
    Signed-off-by: Matias Charriere <matias@giantswarm.io>
    
    * Use template + keep annotation approach
    
    Signed-off-by: Matias Charriere <matias@giantswarm.io>
    
    * remove dep
    
    Signed-off-by: Matias Charriere <matias@giantswarm.io>
    
    * remove dep from readme
    
    Signed-off-by: Matias Charriere <matias@giantswarm.io>
    
    * update changelog
    
    Signed-off-by: Matias Charriere <matias@giantswarm.io>
    
    ---------
    
    Signed-off-by: Matias Charriere <matias@giantswarm.io>

--- sync/patches/crds/patch.sh
+++ sync/patches/crds/patch.sh
@@ -13,5 +13,22 @@ readonly script_dir_rel=".${script_dir#"${repo_dir}"}"
 
 set -x
 rm -rf ./helm/envoy-gateway/crds/gatewayapi-crds.yaml
+mv ./helm/envoy-gateway/crds/generated ./helm/envoy-gateway/templates/crds
+
+templates_path="./helm/envoy-gateway/templates/crds"
+
+set -x
+
+cd "${templates_path}"
+
+{ set +x; } 2>/dev/null
+for f in *.yaml ; do
+  set -x
+  yq -i '.metadata.annotations += {"helm.sh/resource-policy":"keep"}' "$f"
+
+  { set +x; } 2>/dev/null
+done
+
+set -x
 
 { set +x; } 2>/dev/null
